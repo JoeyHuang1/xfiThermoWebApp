09a4bd6fdc2c100ac2fad7c67196eb16333d073b
Joey Huang <JoeyHuangOne@gmai.com>
2018-09-03 20:18:18 -0700

    some debug for Android
    the problem is in server

--- ios/build/Build/Intermediates.noindex/React.build/Debug-iphonesimulator/React.build/Objects-normal/x86_64/RCTFrameUpdate.d
+++ ios/build/Build/Intermediates.noindex/React.build/Debug-iphonesimulator/React.build/Objects-normal/x86_64/RCTFrameUpdate.d
@@ -1,6 +1,4 @@
 dependencies: \
-  /Users/joey/react/native/temp/xfiThermoWebApp/node_modules/react-native/React/Base/RCTFrameUpdate.m \
-  /Users/joey/react/native/temp/xfiThermoWebApp/node_modules/react-native/React/Base/RCTFrameUpdate.h \
-  /Users/joey/react/native/temp/xfiThermoWebApp/node_modules/react-native/React/Base/RCTUtils.h \
-  /Users/joey/react/native/temp/xfiThermoWebApp/node_modules/react-native/React/Base/RCTAssert.h \
-  /Users/joey/react/native/temp/xfiThermoWebApp/node_modules/react-native/React/Base/RCTDefines.h
+  /Users/joey/react/native/comcast/xfiThermoWebApp/node_modules/react-native/React/Base/RCTFrameUpdate.m \
+  /Users/joey/react/native/comcast/xfiThermoWebApp/node_modules/react-native/React/Base/RCTFrameUpdate.h \
+  /Users/joey/react/native/comcast/xfiThermoWebApp/node_modules/react-native/React/Base/RCTUtils.h
